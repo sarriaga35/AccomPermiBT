559da99914c78f4c2d00e1d8c64bb518c44cc77f
sarriaga35 <sebasarriaga@gmail.com>
2024-06-12 20:41:10 -0600

    Version 01

---
+++ app/src/main/java/com/sebas/accompermibt/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.sebas.accompermibt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sebas.accompermibt.ui.theme.AccomPermiBTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AccomPermiBTTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Sample()
+                }
+            }
+        }
+    }
+@Composable
+    private fun Sample() {
+        Text(text = "Hello World!")
+    }
+}
+
+
