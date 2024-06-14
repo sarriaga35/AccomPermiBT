97777d0561bf0553186d37a73bf72733242bc390
sarriaga35 <sebasarriaga@gmail.com>
2024-06-13 21:11:52 -0600

    Version 02.01

--- app/src/main/java/com/sebas/accompermibt/MainActivity.kt
+++ app/src/main/java/com/sebas/accompermibt/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -13,6 +17,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 import com.sebas.accompermibt.ui.theme.AccomPermiBTTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +39,28 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-@Composable
+
+    @OptIn(ExperimentalPermissionsApi::class)
+    @Composable
     private fun Sample() {
-        Text(text = "Hello World!")
+        val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+        if (cameraPermissionState.status.isGranted) {
+            Text("Camera permission Granted")
+        } else {
+            Column {
+                val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
+                    "The camera is important for this app. Please grant the permission."
+                } else {
+                    "Camera not available"
+                }
+
+                Text(textToShow)
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                    Text("Request permission")
+                }
+            }
+        }
     }
 }
 
